a2473d1ada616e3d85a6cc5dd08c367e1864256d
Pranjal Patra <pranjal.cna@gmail.com>
2022-03-04 09:19:00 -0330

    add delete project, emp code and more

--- controller.py
+++ controller.py
@@ -5,57 +5,38 @@ user_name = "root"
 pwd = "password"
 
 
-def add_job(job_class: str, charge_hour: float):
+def execute_and_commit_query(query):
     # Step 1: Create connection object
     with connect(host=hostname, user=user_name, password=pwd) as mysql_connection_object:
         # Step 2: Create a cursor object
         with mysql_connection_object.cursor() as mysql_cursor:
-            # Step 3: Build sql statment
-            add_job_insert_statement = f"""INSERT INTO company.job (job_class, chg_hour) values ('{job_class}', {charge_hour});"""
             # Step 4: Execute the statement
-            mysql_cursor.execute(add_job_insert_statement)
+            mysql_cursor.execute(query)
             # Step 5: Commit Change
             mysql_connection_object.commit()
+
+
+def add_job(job_class: str, charge_hour: float):
+    add_job_insert_statement = f"""INSERT INTO company.job (job_class, chg_hour) 
+                                   values ('{job_class}', {charge_hour});"""
+    execute_and_commit_query(add_job_insert_statement)
 
 
 def add_employee(emp_name, job_class):
-    # Step 1: Create connection object
-    with connect(host=hostname, user=user_name, password=pwd) as mysql_connection_object:
-        # Step 2: Create a cursor object
-        with mysql_connection_object.cursor() as mysql_cursor:
-            # Step 3: Build sql statment
-            add_employee_insert_statement = f"""INSERT INTO company.employee (emp_name, job_class) value('{emp_name}', '{job_class}');"""
-            # Step 4: Execute the statement
-            mysql_cursor.execute(add_employee_insert_statement)
-            # Step 5: Commit Change
-            mysql_connection_object.commit()
+    add_employee_insert_statement = f"""INSERT INTO company.employee (emp_name, job_class) value('{emp_name}', '{job_class}');"""
+    execute_and_commit_query(add_employee_insert_statement)
 
 
 def add_project(project_name):
-    # Step 1: Create connection object
-    with connect(host=hostname, user=user_name, password=pwd) as mysql_connection_object:
-        # Step 2: Create a cursor object
-        with mysql_connection_object.cursor() as mysql_cursor:
-            # Step 3: Build sql statment
-            add_project_insert_statement = f"""INSERT INTO company.project (project_name) value('{project_name}');"""
-            # Step 4: Execute the statement
-            mysql_cursor.execute(add_project_insert_statement)
-            # Step 5: Commit Change
-            mysql_connection_object.commit()
+    add_project_insert_statement = f"""INSERT INTO company.project (project_name) value('{project_name}');"""
+    execute_and_commit_query(add_project_insert_statement)
 
 
 def assign_employee_to_project(employee_num: int, project_num: int, assignment_hours: float):
-    # Step 1: Create connection object
-    with connect(host=hostname, user=user_name, password=pwd) as mysql_connection_object:
-        # Step 2: Create a cursor object
-        with mysql_connection_object.cursor() as mysql_cursor:
-            # Step 3: Build sql statment
-            assign_employee_to_project_statement = f"""INSERT INTO company.assignment (project_num, emp_num, assign_hours) 
-                                                        value({employee_num}, {project_num}, {assignment_hours});"""
-            # Step 4: Execute the statement
-            mysql_cursor.execute(assign_employee_to_project_statement)
-            # Step 5: Commit Change
-            mysql_connection_object.commit()
+    assign_employee_to_project_statement = f"""INSERT INTO company.assignment (project_num, emp_num, assign_hours) 
+                                                            value({project_num}, {employee_num}, {assignment_hours});"""
+    # print(assign_employee_to_project_statement)
+    execute_and_commit_query(assign_employee_to_project_statement)
 
 
 def get_employee_list_for_project(project_name):
@@ -83,14 +64,43 @@ def get_employee_list_for_project(project_name):
     return employee_names
 
 
+def remove_employee(emp_num):
+    # DELETE FROM company.assignment WHERE emp_num = 1;
+    # DELETE FROM company.employee WHERE emp_num = 1;
+    delete_employee_assignments_command = f"DELETE FROM company.assignment WHERE emp_num = {emp_num};"
+    delete_employee_command = f"DELETE FROM company.employee WHERE emp_num = {emp_num};"
+    execute_and_commit_query(delete_employee_assignments_command)
+    execute_and_commit_query(delete_employee_command)
+
+def remove_project(project_id):
+    """
+    Remove project by its ID
+    DELETE FROM company.assignment WHERE project_num = 1;
+    DELETE FROM company.project WHERE project_num = 1;
+    :param project_id: indicates project to be deleted
+    :return: None
+    """
+    del_assignments = f"DELETE FROM company.assignment WHERE project_num = {project_id};"
+    del_project = f"DELETE FROM company.project WHERE project_num = {project_id};"
+    execute_and_commit_query(del_assignments)
+    execute_and_commit_query(del_project)
+
+def remove_employee_from_project(employee_id, project_id):
+    """DELETE FROM company.assignment WHERE project_num = 2 and emp_num = 3;"""
+    remove_employee_from_project_query = f"DELETE FROM company.assignment WHERE project_num = {project_id} and emp_num = {employee_id};"
+    execute_and_commit_query(remove_employee_from_project_query)
+
+
 def testing():
     # add_job(job_class="J002", charge_hour=62.75)
     # add_employee(emp_name='Ron', job_class='J001')
     # add_project('funny_memes')
-    # assign_employee_to_project(1, 1, 2)
-    print(get_employee_list_for_project('funny_memes'))
-    pass
-
+    # assign_employee_to_project(project_num=2, employee_num=3, assignment_hours=5)
+    # print(get_employee_list_for_project('funny_memes'))
+    # remove_employee(emp_num=4)
+    # print(get_employee_list_for_project('funny_memes'))
+    # remove_project(project_id=1)
+    remove_employee_from_project(project_id=2, employee_id=3)
 
 if __name__ == '__main__':
     testing()
